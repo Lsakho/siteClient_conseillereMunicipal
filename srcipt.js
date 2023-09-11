d2c229a2df7c60023903f237b276911b87780b8c
lamine Sakho <laminesakho@macbook-air-de-lamine.home>
2023-09-11 16:09:47 +0200

    #wip new navbar adding darkmode style

--- srcipt.js
+++ srcipt.js
@@ -1,17 +1,28 @@
-$('.navbar-toggler').click(function() {
-    $('.navbar-collapse').slideToggle();
-  });
-  
-  // Animate the navbar icons when a user hovers over them
-  $('.navbar-nav li a').hover(function() {
-    $(this).children('.nav-icon').pulse();
-  });
-  
-  // Fade in the navbar when the page loads
-  $('.navbar').fadeIn(300);
-  
-  // Use a parallax effect to make the navbar appear to move as the user scrolls down the page
-  $(window).scroll(function() {
-    var scrollY = $(window).scrollTop();
-    $('.navbar').css('transform', 'translateY(' + scrollY / 2 + 'px)');
-  });
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+const contactForm = document.querySelector('.contact-form'); // Sélectionnez le formulaire de contact
+const contactText = document.querySelector('.contact-form p'); // Sélectionnez le texte du formulaire de contact
+
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+    contactForm.classList.add('dark-mode'); // Ajoutez la classe au formulaire de contact
+    contactText.classList.add('dark-mode'); // Ajoutez la classe au texte du formulaire de contact
+}
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+        contactForm.classList.add('dark-mode'); // Ajoutez la classe au formulaire de contact
+        contactText.classList.add('dark-mode'); // Ajoutez la classe au texte du formulaire de contact
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+        contactForm.classList.remove('dark-mode'); // Supprimez la classe du formulaire de contact
+        contactText.classList.remove('dark-mode'); // Supprimez la classe du texte du formulaire de contact
+    }
+    console.log("Mode sombre activé :", darkModeToggle.checked);
+
+});
