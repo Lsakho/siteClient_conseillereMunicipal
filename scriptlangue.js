60cef853bda489d699875d2d132be73b3b0b7259
lamine Sakho <laminesakho@macbook-air-de-lamine.home>
2023-09-23 21:52:01 +0200

    adding file from cliet and set up the three national lamguage

---
+++ scriptlangue.js
@@ -0,0 +1,88 @@
+// Initialisation de i18next
+i18next.init({
+    lng: 'fr', // Langue par défaut
+    resources: {
+      fr: {
+        translation: {
+          // Insérez les traductions en français ici
+          
+            "header.title": "Priscille Dia Laplace",
+            "menu.about": "À Propos",
+            "menu.news": "Actualité",
+            "menu.gallery": "Galerie",
+            "menu.program": "Programme",
+            "menu.contact": "Contact",
+            "button.french": "Français",
+            "button.italian": "Italien",
+            "button.german": "Deutsch",
+            "main.title": "Candidate au Conseil national pour le Parti Socialiste à Genève",
+            "main.name": "Priscille Dia-Laplace",
+            "main.municipalCounselor": "Conseillère municipale (PS) Lancy, Genève",
+            "main.teste": "De la musique à la politique en passant par les syndicats. Suite à une carrière internationale de chant qui s’achève en 2015 avec le disque « Myrthen », enregistré avec Fabrizzio Chiovetta à Dobbiaco (Italie) et après être redevenue sédentaire afin de se consacrer à sa famille, Priscille Dia-Laplace réalise la détérioration des services publics à Genève et en Suisse et décide de se syndiquer à la FAMCO (en tant qu’enseignante) et au SSP (en tant que fonctionnaire). Les grandes grèves de 2015 sont l’occasion de manifester contre le délitement du service public et la création de lois iniques telle que « Personal Stop » lancées par une droite de plus en plus agressive et dangereuse selon son point de vue. En 2017, toujours syndiquée et engagée à ce niveau-là, elle décide ensuite d’entrer en politique afin de pouvoir agir en amont et choisit le Parti Socialiste, plus grand parti de gauche en Suisse à être représenté tant au niveau communal, cantonal que fédéral et issu du monde syndical. En 2019, elle est élue au comité directeur du Parti Socialiste à Genève en tant que représentante de l’Assemblée Générale. En 2020, elle devient Conseillère Municipale à Lancy et est actuellement cheffe de groupe des conseillers municipaux de sa commune. En 2022, elle soutient activement la campagne de Thierry Apothéloz qui est réélu en tant que Conseiller d’Etat à Genève. En 2023, elle est candidate au Conseil national et fait campagne aux côtés de ses brillantes et brillants colistières et colistiers : Christian Dandrès, Estelle Revaz, Thomas Wenger, Laurence Fehlmann Rielle, Cyril Mizrahi, Léna Strasser, Sami Kanaan, Joëlle Bertossat, Florian Schweri, Oriana Brücker et Thomas Bruchez."
+        
+        
+        }
+      },
+      it: {
+        translation: 
+          // Insérez les traductions en italien ici
+          {
+            "header.title": "Priscille Dia Laplace",
+            "menu.about": "Chi Sono",
+            "menu.news": "Notizie",
+            "menu.gallery": "Galleria",
+            "menu.program": "Programma",
+            "menu.contact": "Contatti",
+            "button.french": "Francese",
+            "button.italian": "Italiano",
+            "button.german": "Tedesco",
+            "main.title": "Candidata al Consiglio Nazionale per il Partito Socialista a Ginevra",
+            "main.name": "Priscille Dia-Laplace",
+            "main.municipalCounselor": "Consigliera Comunale (PS) Lancy, Ginevra",
+            "main.teste": "Dalla musica alla politica, passando per i sindacati. Dopo una carriera internazionale nel canto che si è conclusa nel 2015 con la registrazione di \"Myrthen\" con Fabrizzio Chiovetta a Dobbiaco (Italia) e dopo essere tornata a una vita più sedentaria per dedicarsi alla sua famiglia, Priscille Dia-Laplace ha notato il deterioramento dei servizi pubblici a Ginevra e in Svizzera e ha deciso di sindacarsi con la FAMCO (come insegnante) e il SSP (come funzionaria). I grandi scioperi del 2015 sono stati l'occasione per protestare contro il declino del servizio pubblico e la creazione di leggi inique come il \"Personal Stop\", promosso da una destra sempre più aggressiva e pericolosa dal suo punto di vista. Nel 2017, ancora sindacata e impegnata a questo livello, ha poi deciso di entrare in politica per poter agire in anticipo e ha scelto il Partito Socialista, il più grande partito di sinistra in Svizzera rappresentato a livello comunale, cantonale e federale ed emerso dal mondo sindacale. Nel 2019 è stata eletta nel comitato direttivo del Partito Socialista a Ginevra come rappresentante dell'Assemblea Generale. Nel 2020 è diventata Consigliera Comunale a Lancy ed è attualmente capogruppo dei consiglieri comunali del suo comune. Nel 2022 ha sostenuto attivamente la campagna di Thierry Apothéloz, che è stato rieletto Consigliere di Stato a Ginevra. Nel 2023 è candidata al Consiglio Nazionale e fa campagna elettorale insieme alle sue brillanti colleghe e ai suoi brillanti colleghi: Christian Dandrès, Estelle Revaz, Thomas Wenger, Laurence Fehlmann Rielle, Cyril Mizrahi, Léna Strasser, Sami Kanaan, Joëlle Bertossat, Florian Schweri, Oriana Brücker e Thomas Bruchez."
+        }
+        
+      },
+      de: {
+        translation: {
+          // Insérez les traductions en allemand ici
+          
+            "header.title": "Priscille Dia Laplace",
+            "menu.about": "Über",
+            "menu.news": "Nachrichten",
+            "menu.gallery": "Galerie",
+            "menu.program": "Programm",
+            "menu.contact": "Kontakt",
+            "button.french": "Französisch",
+            "button.italian": "Italienisch",
+            "button.german": "Deutsch",
+            "main.title": "Kandidatin für den Nationalrat für die Sozialdemokratische Partei in Genf",
+            "main.name": "Priscille Dia-Laplace",
+            "main.municipalCounselor": "Gemeinderätin (SP) Lancy, Genf",
+            "main.teste": "Von Musik über Politik bis hin zu Gewerkschaften. Nach einer internationalen Gesangskarriere, die 2015 mit der Aufnahme von „Myrthen“ mit Fabrizzio Chiovetta in Dobbiaco (Italien) endete und nachdem sie sich wieder sesshaft gemacht hatte, um sich ihrer Familie zu widmen, erkannte Priscille Dia-Laplace den Verfall der öffentlichen Dienstleistungen in Genf und der Schweiz und beschloss, sich bei der FAMCO (als Lehrerin) und beim SSP (als Beamtin) zu organisieren. Die großen Streiks von 2015 waren Anlass, gegen den Abbau des öffentlichen Dienstes und die Schaffung ungerechter Gesetze wie dem „Personal Stop“, die von einer immer aggressiveren und gefährlicheren Rechten aus ihrer Sicht ins Leben gerufen wurden, zu demonstrieren. 2017, immer noch organisiert und auf diesem Niveau engagiert, beschloss sie dann, in die Politik einzusteigen, um im Vorfeld handeln zu können, und wählte die Sozialdemokratische Partei, die größte linke Partei in der Schweiz, die auf kommunaler, kantonaler und föderaler Ebene vertreten ist und aus der Gewerkschaftsbewegung stammt. 2019 wurde sie in den Vorstand der Sozialdemokratischen Partei in Genf gewählt und vertritt die Generalversammlung. 2020 wurde sie Gemeinderätin in Lancy und ist derzeit Fraktionsvorsitzende der Gemeinderäte ihrer Gemeinde. 2022 unterstützte sie aktiv die Kampagne von Thierry Apothéloz, der als Staatsrat in Genf wiedergewählt wurde. 2023 kandidiert sie für den Nationalrat und macht Wahlkampf an der Seite ihrer brillanten Mitkandidatinnen und Mitkandidaten: Christian Dandrès, Estelle Revaz, Thomas Wenger, Laurence Fehlmann Rielle, Cyril Mizrahi, Léna Strasser, Sami Kanaan, Joëlle Bertossat, Florian Schweri, Oriana Brücker und Thomas Bruchez."
+        
+        }
+      }
+    }
+  });
+  
+  document.querySelectorAll('.custom-button').forEach(button => {
+    button.addEventListener('click', (event) => {
+      const newLang = event.target.getAttribute('data-lang');
+      i18next.changeLanguage(newLang, (err, t) => {
+        document.body.querySelectorAll('[data-i18n]').forEach(element => {
+          const key = element.getAttribute('data-i18n');
+          element.textContent = t(key);
+        });
+      });
+    });
+  });
+  
+  
+  i18next.on('initialized', () => {
+    document.body.querySelectorAll('[data-i18n]').forEach(element => {
+      const key = element.getAttribute('data-i18n');
+      element.textContent = i18next.t(key);
+    });
+  });
+  
